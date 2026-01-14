a31674a4d65f23628ebe2d40b16b4bf2beedbf10
maidegarcia <maide.garcia11@gmail.com>
2026-01-13 16:38:18 -0800

    Snowman Face
    Added the snowman's face: eyes, mouth and nose

--- classwork/01_basic_drawing_practice_starter/main.js
+++ classwork/01_basic_drawing_practice_starter/main.js
@@ -1,6 +1,6 @@
 "use strict"
 
-let drawingWidth = 500;
+let drawingWidth = 500; //This determines the size for our canvas
 let drawingHeight = 500;
 
 /*  Variable that enables you to "talk to" your SVG drawing canvas. */
@@ -26,12 +26,38 @@ let snowmanHead = drawing.append("circle")
 
 /* Exercise: Draw the left eye; store it in an appropriately named variable */
 
+let snowmanLeftEye = drawing.append("circle")
+    .attr("cx", 35)
+    .attr("cy", 35)
+    .attr("r", 5)
+    .attr("fill", "black");
 
 /* Exercise: Draw the right eye; name this variable consistently with your others */
 
+let snowmanRightEye = drawing.append("circle")
+    .attr("cx", 65)
+    .attr("cy", 35)
+    .attr("r", 5)
+    .attr("fill", "black");
+
 /* Exercise: Draw the mouth by appending a line (and store it in a variable) */
+
+let snowmanMouth = drawing.append("line")
+    .attr("x1", 30)
+    .attr("y1", 65)
+    .attr("x2", 70)
+    .attr("y2", 65)
+    .attr("stroke", "black")
+    .attr("stroke-with", 2);
+
 
 /* Exercise: Draw the nose by appending a polyline
     and using the closedPolygon function that we provided you
     (and storing it in a variable) */
     
+let snowmanNose = drawing.append("polyline")
+    .attr("points", closedPolygon(50, 35,
+                                 45, 45,
+                                 70, 75
+    ))
+    .attr("fill", "orange");
