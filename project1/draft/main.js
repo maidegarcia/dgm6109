e5dfab910f96319b1399adf1d5828377d7a9eceb
maidegarcia <maide.garcia11@gmail.com>
2026-01-13 19:36:35 -0800

    Project 1 Draft
    Added a folder for project 1 that contains the drawing starter for the draft

---
+++ project1/draft/main.js
@@ -0,0 +1,20 @@
+"use strict"
+
+/*  Variable that enables you to "talk to" your SVG drawing canvas. */
+let drawing = d3.select("#canvas")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 500);
+
+/* Draw a border that matches the maximum drawing area for this assignment.
+    Assign the border to a variable so that:
+        (1) We know what the purpose of the shape is, and
+        (2) We will have the ability to change it later (in a future assignment)
+*/
+let border = drawing.append("rect")
+    .attr("width", 500)
+    .attr("height", 500)
+    .attr("fill", "none")
+    .attr("stroke", "red");
+
+/* Write your code for Project 1 beneath this comment */
