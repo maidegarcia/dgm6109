5779408fff47914eefc64b0cf849150d609d2517
maidegarcia <maide.garcia11@gmail.com>
2026-01-22 20:00:39 -0800

    Lab 3
    Created lab3

---
+++ lab3/temp-convertion.js
@@ -0,0 +1,7 @@
+let fahrenheit = 66.2 // This was the average temperature in Bogotá Colombia on August 2025
+let celsius = (fahrenheit - 32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console.log("Temperature (fahrenheit):" + fahrenheit)
+console.log("Temperature (celsius):" + celsius)
+console.log("Temperature (kelvin):" + kelvin)
