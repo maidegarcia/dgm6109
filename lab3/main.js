dcc1d410996d7534eb2e0401d7d679aefe5438fc
maidegarcia <maide.garcia11@gmail.com>
2026-01-22 21:30:41 -0800

    Kelvin problem solved
    the value was interpreted as a string, I had to convert it

--- lab3/main.js
+++ lab3/main.js
@@ -2,10 +2,12 @@
 
 document.getElementById("submit").addEventListener("click", function () {
 
-    let fahrenheit = document.getElementById("inputF").value;
+    let fahrenheit = document.getElementById("inputF").value; 
     let celsius = (fahrenheit - 32) * 5 / 9
-    let kelvin = (fahrenheit + 459.67) * 5 / 9
-
+    let kelvin = (Number(fahrenheit )+ 459.67) * 5 / 9 //the document.getElementId("inputF").value,changes 
+                                                       // the number to a String value and that is why you have to convert it.
+                                                       //You can see it in the DOM inspector, the value appers quotation marks. 
+                                                       //It doesn't seem to affect subtractions
     console.log("Temperature (fahrenheit):" + fahrenheit)
     console.log("Temperature (celsius):" + celsius)
     console.log("Temperature (kelvin):" + kelvin)
