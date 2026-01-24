61d94a6d423b19d952ca033ae415bf138cb2adb8
maidegarcia <maide.garcia11@gmail.com>
2026-01-23 16:36:20 -0800

    Project 2
    Added the folder project with the validation data already done

---
+++ project2/draft/main.js
@@ -0,0 +1,90 @@
+"use strict"
+
+/* IC: Declare global variables to store user-entered information and results here */
+let routingNumber, transactionType, transactionLocation;
+
+/* IC: We have set up the form buttons for you, as well as the code that will clear the output once the user has successfully filled out the form and the program has given them final output (instead of telling them they need to correct something). We have set things up so that the output area is cleared, but the form remains filled out. This is to make it easier to test your project with slightly different versions of information rather than having to fill out the whole form every time. */
+
+document.getElementById("submit").addEventListener("click", processForm);
+
+document.getElementById("reset").addEventListener("click", function () {
+    clear();
+    document.getElementById("submit").toggleAttribute("hidden");
+    document.getElementById("reset").toggleAttribute("hidden");
+});
+
+function processForm() {
+
+    /* IC: Assign values from your form inputs here, remembering:
+        1) Always work with the value property from the form input
+
+        2) Form data always comes in as type String. You MAY want to convert some inputs to Numbers, but you ALSO may need to analyze some numeric inputs as text (for example, if you need to check how many digits were entered, or only look at certain digits)
+
+        3) You can do additional pre-processing here, if needed, but everything related to validating form input or providing results should go into the other functions provided below OR by functions that those other functions call (which you may also write)
+    */
+
+    routingNumber = document.getElementById("number").value;
+    transactionType = document.getElementById("type").value;
+    transactionLocation = document.getElementById("location").value
+
+    /* IC: This code looks for a true or false for whether the data is valid. It only continues to evaluate the answers if the data is valid. You DO NOT need to modify any code between here and the end of the function, nor should you, unless you have a good reason. All versions of this project can be completed WITHOUT modifying the code from this comment to the end of the function, so you should attempt to work with that restriction! */
+
+    let evaluationCompleted = false;
+
+    if (validateData()) {
+        evaluationCompleted = evaluateAnswers();
+    }
+
+    if (evaluationCompleted) {
+        document.getElementById("submit").toggleAttribute("hidden");
+        document.getElementById("reset").toggleAttribute("hidden");
+    } else {
+        rule();
+    }
+}
+
+/* IC: In this function, do any validation with validate the data was correctly entered in general, not for specific cases. Return false if you have told the user that they need to correct something. Return true if all data is valid. We have provided you with the basic constraints for the data, but you may improve the validation as a bonus (as long as you don't mess up our ability to test every option in your evaluateAnswers function!) */
+
+function validateData() {
+    let valid = true;
+
+    if (routingNumber.length <9 || routingNumber.length > 9) {
+        output("Please enter a 9 digits bank routing number!")
+        valid = false;
+    }
+
+    if(transactionType=="0"){
+        output("Please select a type of transaction!")
+        valid = false;
+    }
+
+    if(transactionLocation=="0"){
+        output("Please select a transaction location!")
+        valid = false;
+    }
+
+    if(valid==true){
+        output("All form data is valid")
+    }
+    
+    return valid;
+
+  
+    
+    
+}
+
+/* IC: In this function, use conditional logic to figure out if the user's input meets all of the constraints that we have provided. Return false if you have told the user that they need to correct something. Return true if all data is valid. NOTE: Although the focuses of this project are conditional logic and function returns, you may need to create additional variables, do some calculations, and/or do some String manipulation in order to successfully complete your project! */
+
+function evaluateAnswers() {
+
+    if (amount % choice != 0) {
+        output(`Sorry, your quantity ${amount} must be evenly divisible by the option you chose (you chose ${choice}!`);
+        return false;
+    }
+
+    output ("Sure, you can have " + amount + " of " + choice + ".")
+    return true;
+}
+
+/* TIP: The above two functions are written using different techniques for communicating success or failure. In your project, we will be looking for consistency -- i.e., choose ONE of these methods (early returns, or tracking the success in a variable) and use it throughout your project! */
