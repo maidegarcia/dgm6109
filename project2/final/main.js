a0ce912ffd86c16cef6514fc1c851ccb86b97b06
maidegarcia <maide.garcia11@gmail.com>
2026-01-30 17:34:02 -0800

    Explanation comments
    Added more explanation comments and changed some values in the html to facilitate the generation of the final message

--- project2/final/main.js
+++ project2/final/main.js
@@ -1,11 +1,8 @@
 "use strict"
 
-/* IC: Declare global variables to store user-entered information and results here */
-let routingNumber, transactionTypeValue, transactionTypeText, transactionLocationValue, transactionLocationText, confirmationCode, partialView, a, b, c;
+let routingNumber, transactionType, transactionLocation, confirmationCode, partialView;
 
-/* IC: We have set up the form buttons for you, as well as the code that will clear the output once the user has successfully filled out the form and the program has given them final output (instead of telling them they need to correct something). We have set things up so that the output area is cleared, but the form remains filled out. This is to make it easier to test your project with slightly different versions of information rather than having to fill out the whole form every time. */
-
-document.getElementById("submit").addEventListener("click", processForm);
+document.getElementById("submit").addEventListener("click", processFormValues);
 
 document.getElementById("reset").addEventListener("click", function () {
     clear();
@@ -14,23 +11,16 @@ document.getElementById("reset").addEventListener("click", function () {
     document.getElementById("reset").toggleAttribute("hidden");
 });
 
-function processForm() {
-
-    /* IC: Assign values from your form inputs here, remembering:
-        1) Always work with the value property from the form input
-
-        2) Form data always comes in as type String. You MAY want to convert some inputs to Numbers, but you ALSO may need to analyze some numeric inputs as text (for example, if you need to check how many digits were entered, or only look at certain digits)
-
-        3) You can do additional pre-processing here, if needed, but everything related to validating form input or providing results should go into the other functions provided below OR by functions that those other functions call (which you may also write)
-    */
+function processFormValues() { //This function is to get the values from the user input
 
     routingNumber = document.getElementById("number").value;
-    transactionTypeValue = document.getElementById("type").value;
-    //transactionTypeText = transactionTypeValue.options[transactionTypeValue.selectedIndex].text;
-    transactionLocationValue = document.getElementById("location").value;
-    //transactionLocationText = transactionLocationValue.options[transactionLocationValue.selectedIndex].text;
+    transactionType = document.getElementById("type").value;
+    transactionLocation = document.getElementById("location").value;
 
-    /* IC: This code looks for a true or false for whether the data is valid. It only continues to evaluate the answers if the data is valid. You DO NOT need to modify any code between here and the end of the function, nor should you, unless you have a good reason. All versions of this project can be completed WITHOUT modifying the code from this comment to the end of the function, so you should attempt to work with that restriction! */
+    processData();
+}
+
+function processData() { //This function passes information on to the functions validateData() and evaluateAnswers
 
     let evaluationCompleted = false;
 
@@ -48,21 +38,22 @@ function processForm() {
 
 
 
-/* IC: In this function, do any validation with validate the data was correctly entered in general, not for specific cases. Return false if you have told the user that they need to correct something. Return true if all data is valid. We have provided you with the basic constraints for the data, but you may improve the validation as a bonus (as long as you don't mess up our ability to test every option in your evaluateAnswers function!) */
 
-/*For this function I decided to use the if conditional logic because of my thinking process, in this step
-I prefer to evaluate each type of data separately. We are not in a step that is evaluating the input
-values as a whole, just checking if each value is following the parameters needed to start evaluating 
-the data*/
+/* validateData() is to check if the data was correctly entered in general, not for specific cases. Each condition 
+returns false if there is somethimg incorrect with the data, if the data is ok, they will return true 
+
+For this function I decided to use the if conditional logic because of my thinking process, in this step I prefer to 
+evaluate each type of data and condition separately.*/
 
 function validateData() {
 
     let valid = true;
 
-    /*This is still a work in progress but is meant to analyze if the user entered a number */
+    /*This analyzes if the user entered a number. If they didn't, the system will ask them to put a number. For this I used isNaN()
+    tha checks if a number is a NaN(not a number). I learned that here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN */
 
-    if (Number.isNaN(Number(routingNumber))) {
-        output("Please enter a number!");
+    if (isNaN(Number(routingNumber))) {
+        output("Please enter ONLY numbers in the bank routing number box!");
         valid = false;
     }
 
@@ -78,10 +69,10 @@ function validateData() {
     }
 
     /*Here I'm saying that if the transaction type equals to "0", the system will output a message. If you
-    go to the html, you will see that"0"is an empty option, I put it so that when the user enters the page,
+    go to the html, you will see that "0"is an empty option, I put it so that when the user enters the page,
     they see everything in blank, it was a design decision*/
 
-    if (transactionTypeValue == "0") {
+    if (transactionType == "0") {
         output("Please select a type of transaction!")
         valid = false;
     }
@@ -89,7 +80,7 @@ function validateData() {
     /*For this conditional I used the exact same logic that in the previous one, if the transaction location
     equals "0", an empty option, the system will output a message*/
 
-    if (transactionLocationValue == "0") {
+    if (transactionLocation == "0") {
         output("Please select a transaction location!")
         valid = false;
     }
@@ -98,74 +89,85 @@ function validateData() {
 
 }
 
-/* IC: In this function, use conditional logic to figure out if the user's input meets all of the constraints that we have provided. Return false if you have told the user that they need to correct something. Return true if all data is valid. NOTE: Although the focuses of this project are conditional logic and function returns, you may need to create additional variables, do some calculations, and/or do some String manipulation in order to successfully complete your project! */
+/* This function evaluates if the user's input meets all of the constraints. Each conditional will return false if one of the 
+constraints is not met and the system will output an error message. If everything is correct, they will return true and the system 
+will create an output message with the result of the transaction. 
 
-/*For this function I used the if/else if conditional logic because in this case I'm evaluating
-all three types of input data together, I'm seeing them as a whole unit that can appear in different
-specific combinations.  */
+For this function I used the if/else if conditional logic because in this case I'm evaluating all three types of input data together, 
+I'm seeing them as a whole unit that can appear in different specific combinations. */
 
 function evaluateAnswers() {
 
     let valid = true;
 
 
-    /*In this part I'm saying that if the user chose the first option and their routing number is not equal to
+    /*In this part I'm saying that if the user chose the transaction Treasury Check and their routing number is not equal to
      "000000518", the system has to output an error message*/
 
-    if (transactionTypeValue == "Treasury check" && routingNumber != "000000518") {
-        output("The routing number is incorrect for this type of transaction")
+    if (transactionType == "Treasury Check" && routingNumber != "000000518") {
+        output("The routing number is incorrect for this type of transaction!")
         valid = false;
     }
 
-    /*In this part I'm saying that if the user chose the second option and their routing number is not equal to
+    /*In this part I'm saying that if the user chose the transaction Money Order and their routing number is not equal to
      any of those options, the system has to output an error message*/
 
-    else if (transactionTypeValue == "2" && routingNumber != "000000204" && routingNumber != "000001193" && routingNumber != "000008002") {
-        output("The routing number is incorrect for this type of transaction")
+    else if (transactionType == "Money Order" && routingNumber != "000000204" && routingNumber != "000001193" && routingNumber != "000008002") {
+        output("The routing number is incorrect for this type of transaction!")
         valid = false;
     }
 
-    /*In this part I'm saying that if the user chose the third option and their routing number is not equal to
+    /*In this part I'm saying that if the user chose the transaction Savings Bond and their routing number is not equal to
      "000090007", the the system has to output an error message*/
 
-    else if (transactionTypeValue == "3" && routingNumber != "000090007") {
-        output("The routing number is incorrect for this type of transaction")
+    else if (transactionType == "Savings Bond" && routingNumber != "000090007") {
+        output("The routing number is incorrect for this type of transaction!")
         valid = false;
     }
 
-    /*In the next three conditionals I'm evaluating the different options that need to be true if the 
-    user chose the fourth transaction type, the first, second or third transaction locations and if the 
-    first 4 digits of the number they entered correspond to the ones assigned to each location.*/
+    /*The next three conditionals are to evaluate when the user wants to do a Local Deposit. I'm also evaluating 
+    the transaction location and if the routing number does not correspond to the ones assigned to each location.
 
-    /*I could have put in one line the "else if (transactionType == "4"" and then nested the other 
+    I could have put in one line the "else if (transactionType == "4")" and then nested the other 
     values that are the ones that change, but I prefered each option to have its own line of code, 
-    it was easier for me visually to have it like it*/
+    it was easier for me visually to have it like that.
 
-    /*For this conditionals I had to use .slice(xn, yn), to be able to just evaluate the first four
-    digits of the number. What this does is that it slices the string from the start character, in
+    For this conditionals I had to use .slice(xn, yn), to be able to just evaluate the first four
+    digits of the number. What this does is that it slices the string from the indicated character, in
     this case digit, that you choose (xn) to the end that you decide, that would be yn, but yn is not included 
-    in the new string that will result out of this. I read about the string processing in the web page
-    https://javascript.info/string */
+    in the new string that will result out of this. It is important to say that in this processes, the first character 
+    is "0" and not "1". I read about the string processing in the web page
+    https://javascript.info/string  .*/
 
-    else if (transactionTypeValue == "4" && transactionLocationValue == "1" && routingNumber.slice(0, 4) != "0410" && routingNumber.slice(0, 4) != "0412") {
-        output("The routing number is incorrect for this type of transaction")
+    else if (transactionType == "Local Deposit" && transactionLocation == "Cleveland,OH" && routingNumber.slice(0, 4) != "0410" && routingNumber.slice(0, 4) != "0412") {
+        output("The routing number is incorrect for this type of transaction!")
         valid = false
     }
 
-    else if (transactionTypeValue == "4" && transactionLocationValue == "2" && routingNumber.slice(0, 4) != "0711") {
-        output("The routing number is incorrect for this type of transaction")
+    else if (transactionType == "Local Deposit" && transactionLocation == "Peoria, IL" && routingNumber.slice(0, 4) != "0711") {
+        output("The routing number is incorrect for this type of transaction!")
 
         valid = false;
     }
 
-    else if (transactionTypeValue == "4" && transactionLocationValue == "3" && routingNumber.slice(0, 4) != "0710" && routingNumber.slice(0, 4) != "0712" && routingNumber.slice(0, 4) != "0719") {
-        output("The routing number is incorrect for this type of transaction")
+    else if (transactionType == "Local Deposit" && transactionLocation == "Chigago, IL" && routingNumber.slice(0, 4) != "0710" && routingNumber.slice(0, 4) != "0712" && routingNumber.slice(0, 4) != "0719") {
+        output("The routing number is incorrect for this type of transaction!")
         valid = false;
 
     }
 
+    /*This conditional is to create a 6 digits confirmation code in case the data is valid. I had to convert the String values of the routing
+    number to Number values, in order to do calculations and get a number. The idea is to think about the routing number as abc|abc|abc, the 
+    sum of each set of a, b and c is going to create the confirmation code. Since sometimes the result is a one digit number, I'm
+    putting a conditional saying that, if the resulting number is less than 10, the system has to add a zero in front of it. I put 
+    zero as a String, because if I left it as a number I would get the same number with which I started. 
+    
+    I used another String processing method the str[] and is to get a specific character of the string. I learned this during class #4
+    but you can also find it here https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Useful_string_methods */
 
     if (valid == true) {
+
+        let a, b, c;
 
         a = Number(routingNumber[0]) + Number(routingNumber[3]) + Number(routingNumber[6])
         if (a < 10) {
@@ -181,18 +183,25 @@ function evaluateAnswers() {
         if (c < 10) {
             c = "0" + c
         }
+        /*This is to create a partial view of the routing number */
+        partialView = "XXXX-X" + routingNumber.slice(5, 8) + "-" + routingNumber[8];
 
-        partialView = "XXXX-X" + routingNumber.slice(5, 8) + "-" + routingNumber.slice(8, 9);
-        output("Your " + transactionTypeValue + " has been iniated at our branch in " + transactionLocationValue + " using routing number " + partialView + " (only the last 4 digits are shown, for security purposes). Your confirmation code is " + a + b + c + ".")
-
-
-
+        /*This is to create a message with the transaction's result */
+        output("Your " + transactionType + " has been iniated at our branch in " + transactionLocation + " using routing number " + partialView + " (only the last 4 digits are shown, for security purposes). Your confirmation code is " + a + b + c + ".")
     }
-
     return valid;
 
 
 }
 
-/* TIP: The above two functions are written using different techniques for communicating success or failure. In your project, we will be looking for consistency -- i.e., choose ONE of these methods (early returns, or tracking the success in a variable) and use it throughout your project! */
+
+
+
+
+
+
+
+
+
+
 
