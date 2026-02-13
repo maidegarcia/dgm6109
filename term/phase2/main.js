9e64b1e9682e4ce4730a0614929a3c57634af063
maidegarcia <maide.garcia11@gmail.com>
2026-02-13 15:02:04 -0800

    phase 2
    started the phase 2 of the project

---
+++ term/phase2/main.js
@@ -0,0 +1,38 @@
+"use strict"
+
+let yourVariableHere = [{
+    studentName: "Susie",
+    numberOfFriends: 3,
+    namesOfEnemies: ["Terry", "Allison"]
+}, {
+    studentName: "Terry",
+    numberOfFriends: 4,
+    namesOfEnemies: ["Susie", "Jamie", "Horace"]
+}, {
+    studentName: "Horace",
+    numberOfFriends: 2,
+    namesOfEnemies: ["Susie"]
+}];
+
+
+let days = [{
+    date: "Monday 26 Jan 2026",
+    studySession: [{time:"3pm", duration:30}, {time:"4pm", duration:180}],
+    musicSession: {time:"3:40pm", duration:20},
+    frenchSession: [{time:"11am", duration:30}, {time:"7pm", duration:40}],
+    readSession:[{time:"9pm", duration:30}],
+    sleepSession: {time: "11pm", duration:455},
+    energergyLevel:[{time:"7am", energy:4}, {time:"7pm", energy:4}]
+
+}, {
+    studentName: "Terry",
+    numberOfFriends: 4,
+    namesOfEnemies: ["Susie", "Jamie", "Horace"]
+}, {
+    studentName: "Horace",
+    numberOfFriends: 2,
+    namesOfEnemies: ["Susie"]
+}];
+
+// console.log(JSON.stringify(yourVariableHere));
+// showData(yourVariableHere);
